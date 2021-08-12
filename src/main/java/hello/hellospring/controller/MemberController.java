212141bee97b4c3fffa8279a7cd922b401502809
hwany <trin_@naver.com>
2021-08-12 13:33:33 +0900

    use H2 database and change memory to jdbcMemberRepository

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -25,7 +25,7 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
-    @PostMapping("/member/new")
+    @PostMapping("/members/new")
     public String create(MemberForm form){
         Member member = new Member();
         member.setName(form.getName());
@@ -33,6 +33,7 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/";
     }
+
 
     @GetMapping("/members")
     public String list(Model model){
